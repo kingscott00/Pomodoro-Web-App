611b73236443c3fa49c8ef7f72c85ccf1b3574f4
Scott King <kingscott00@gmail.com>
2015-12-20 21:26:16 -0800

    added timer functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,54 @@
-window.onload = function(){
-	document.getElementById("playPauseButton").addEventListener("click", playPauseButtonClick);
+var bTimerStarted = false;
+var bTimerOn = false;
+var cnt = 0;
+
+window.onload = function () {
+    bTimerStarted = false;
+    bTimerOn = false;
+    cnt = 0;
+
+    document.getElementById("playPauseButton").addEventListener("click", playPauseButtonClick);
 }
 
-function playPauseButtonClick(){
-	var buttonIcon = document.getElementById("playPauseButtonIcon");
-	if (buttonIcon.innerHTML == "play_arrow") {
-		buttonIcon.innerHTML = "pause"
-	}else{
-			buttonIcon.innerHTML = "play_arrow";
-		}
+function playPauseButtonClick() {
+    var buttonIcon = document.getElementById("playPauseButtonIcon");
+    var button = document.getElementById("playPauseButton");
+
+    if (buttonIcon.innerHTML == "play_arrow") {
+        buttonIcon.innerHTML = "pause"
+        button.classList.remove("green");
+        button.classList.add("red");
+        toggleTimer();
+    } else {
+        buttonIcon.innerHTML = "play_arrow";
+        button.classList.remove("red");
+        button.classList.add("green");
+        toggleTimer();
+    }
+}
+
+function toggleTimer() {
+    if (bTimerStarted == false) {
+        setInterval(updateTimer, 1000);
+        bTimerStarted = true;
+        bTimerOn = true;
+    }
+
+    if (bTimerStarted == true) {
+        if (bTimerOn == true) {
+            clearInterval();
+            bTimerOn = false;
+        }else{
+            setInterval(updateTimer, 1000);
+            bTimerOn = true;
+        }
+    }
+}
+
+
+
+function updateTimer() {
+    var timer = document.getElementById("timer");
+    cnt = cnt + 1;
+    timer.innerHTML = cnt.toString();
 }
